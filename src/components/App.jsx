db274a2133274cdf5a5c7951a86b816d937dcc69
TetianaTitarenko <110638769+TetianaTitarenko@users.noreply.github.com>
2023-03-15 01:25:14 +0000

    Hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,46 @@
-import { Component } from "react";
+import { useState } from "react";
 import Section from "./Section/Section";
 import Button from "./Button/Button";
 import Statistics from "./Statistics/Statistics";
 import Notification from "./Notification/Notification";
 
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  };
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-addReviewGood = () => {this.setState(prevState => ({good: prevState.good +1}))};
+  const addReviewGood = () => { setGood(prevGood => prevGood + 1) };
 
-addReviewNeutral = () => {this.setState(prevState => ({neutral: prevState.neutral +1}))};
+  const addReviewNeutral = () => { setNeutral(prevNeutral => prevNeutral + 1) };
 
-addReviewBad = () => {this.setState(prevState => ({bad: prevState.bad +1}))};
+  const addReviewBad = () => { setBad(prevBad => prevBad + 1) };
 
-render() {
-  const total = this.state.good + this.state.neutral + this.state.bad;
-  const positive = (this.state.good / total*100).toFixed(0)
+  const total = good + neutral + bad;
+  const positive = (good / total * 100).toFixed(0)
   return (
     <div>
       <Section title="Please leave feedback">
-        <Button 
-          onGood={this.addReviewGood} 
-          onNeutral={this.addReviewNeutral} 
-          onBad={this.addReviewBad}>
+        <Button
+          onGood={addReviewGood}
+          onNeutral={addReviewNeutral}
+          onBad={addReviewBad}>
         </Button>
       </Section>
       {total > 0 ? (
-      <Section title="Statistics">
-        <Statistics 
-          good={this.state.good} 
-          neutral={this.state.neutral} 
-          bad={this.state.bad} total={total} 
-          positive={positive}>          
-        </Statistics>
-      </Section>) : 
-      (<Section title="Statistics">
-        <Notification message="There is no feedback" />
-      </Section>)}
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad} total={total}
+            positive={positive}>
+          </Statistics>
+        </Section>) :
+        (<Section title="Statistics">
+          <Notification message="There is no feedback" />
+        </Section>)}
     </div>
   );
 };
-};
 
 export default App
-
